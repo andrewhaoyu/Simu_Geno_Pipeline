c5f6d371b003125aa75914cf14a0ebd8a57b1212
Zhang <andrew.haoyu@gmail.com>
2023-01-31 22:32:41 -0500

    update

--- code/prepare_1KG/2_process_hap_file.R
+++ code/prepare_1KG/2_process_hap_file.R
@@ -16,11 +16,11 @@ args <- commandArgs(trailingOnly = T)
 i = as.numeric(args[[1]])
 j = as.numeric(args[[2]])
 eth_vec = c("EUR", "AFR", "AMR", "EAS", "SAS")
-system(paste0("cd /data/zhangh24/Simu_Geno_Pipeline/result/hap/",eth_vec[i],"/; unzip ",eth_vec[i],"_chr",j,".hap.zip"))
-system(paste0("cd /data/zhangh24/Simu_Geno_Pipeline/result/hap/",eth_vec[i],"/; rm ",eth_vec[i],"_chr",j,".hap.zip"))
-system(paste0("cd /data/zhangh24/Simu_Geno_Pipeline/result/hap/",eth_vec[i],"/; mv chr",j,".hap ",eth_vec[i],"_chr",j,".hap"))
-system(paste0("cd /data/zhangh24/Simu_Geno_Pipeline/result/hap/",eth_vec[i],"/; zip ",eth_vec[i],"_chr",j,".hap ",eth_vec[i],"_chr",j,".hap.zip"))
-
+# system(paste0("cd /data/zhangh24/Simu_Geno_Pipeline/result/hap/",eth_vec[i],"/; unzip ",eth_vec[i],"_chr",j,".hap.zip"))
+# system(paste0("cd /data/zhangh24/Simu_Geno_Pipeline/result/hap/",eth_vec[i],"/; rm ",eth_vec[i],"_chr",j,".hap.zip"))
+# system(paste0("cd /data/zhangh24/Simu_Geno_Pipeline/result/hap/",eth_vec[i],"/; mv chr",j,".hap ",eth_vec[i],"_chr",j,".hap"))
+system(paste0("cd /data/zhangh24/Simu_Geno_Pipeline/result/hap/",eth_vec[i],"/; zip ",eth_vec[i],"_chr",j,".hap.zip ",eth_vec[i],"_chr",j,".hap"))
+system(paste0("cd /data/zhangh24/Simu_Geno_Pipeline/result/hap/",eth_vec[i],"/; rm chr",j,".hap"))
 # for(i in 1:5){
 #   for(j in 1:22){
     #system(paste0("cd /data/zhangh24/KG.impute2/",eth_vec[i],"; cp chr",j,".hap /data/zhangh24/Simu_Geno_Pipeline/result/hap/",eth_vec[i]))
